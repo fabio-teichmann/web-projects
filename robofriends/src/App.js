49d4c3db6b590013419036ccac837ce84c68204c
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-01-23 10:00:07 +0200

    functional vs. class components

---
+++ robofriends/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+// CLASS COMPONENT (vs. functional component)
+// used to be like this back in the days
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
