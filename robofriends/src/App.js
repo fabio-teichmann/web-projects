45012fd73ab1a7dc9f9141481aaac19ed2cdca3c
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-01-24 09:15:07 +0200

    added fetch from API for robots

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
 import CardList from "./CardList";
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+// import { robots } from './robots';
 import './App.css';
 
-
+// smart component, since it has STATE
 class App extends React.Component {
     // state needs to be assigned in constructor
     constructor() {
         super();
         this.state = {
             // can change
-            robots: robots,
+            robots: [],
             searchfield: ''
         };
     }
+
+    // part of React --> no arrow function
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }));
+    }
+
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
     }
@@ -24,13 +32,18 @@ class App extends React.Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         });
 
-        return (
-            <div className="tc">
-                <h1 className="f2">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filterRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 className="tc">Loading...</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f2">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filterRobots}/>
+                </div>
+            );
+        }
+        
     }
 }
 
