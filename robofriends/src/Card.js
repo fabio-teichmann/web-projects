c28b956929f8aab44be7620ab7cc296ab98ed823
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-01-24 09:15:47 +0200

    added delete button to card; onClick action still missing

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,10 +1,15 @@
 import React from "react";
+import './Card.css';
 
 // destructuring directly at recieving
 const Card = ({id, name, email}) => {
     // const {id, name, email} = props;
     return (
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <div>
+            <button type="button">X</button>
+            </div>
+            {/* <br></br> */}
             <img alt='robots' src={`https://robohash.org/${id}`} height={200}/>
             <div>
                 <h2>{name}</h2>
