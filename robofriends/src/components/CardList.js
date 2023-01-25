ca4e7f864c549db08f2ac6e4160028fb13a1a00d
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-01-25 22:52:51 +0200

    amended CardList component to switch between robots and Star Wars

--- robofriends/src/components/CardList.js
+++ robofriends/src/components/CardList.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Card from './Card';
+import SWCard from './SWCard';
 
 const CardList = ({robots}) => {
+    // if (true) {
+    //     throw new Error('NOOOOOOOO');
+    // }
     return (
         <div>
             {
@@ -9,9 +13,11 @@ const CardList = ({robots}) => {
                     return (
                     <Card 
                         key={i} 
-                        id={robots[i].id} 
+                        id={i}
+                        // id={robots[i].id} 
                         name={robots[i].name} 
-                        email={robots[i].email}
+                        email={robots[i].gender}
+                        // email={robots[i].email}
                     />
                     );
                 })
@@ -20,4 +26,44 @@ const CardList = ({robots}) => {
     );
 }
 
-export default CardList
+const CardList2 = ({ users, mode }) => {
+    if (mode === "robots") {
+        return (
+            <div>
+                {
+                    users.map((user, i) => {
+                        return (
+                        <Card 
+                            key={i} 
+                            id={users[i].id} 
+                            name={users[i].name} 
+                            email={users[i].email}
+                        />
+                        );
+                    })
+                }
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                {
+                    users.map((user, i) => {
+                        return (
+                            <SWCard 
+                                key={i} 
+                                id={i} 
+                                name={users[i].name} 
+                                birthYear={users[i].birth_year}
+                                eyeColor={users[i].eye_color}
+                            />
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+    
+}
+
+export { CardList, CardList2};
