5a042b7a1394d8ae88df5be60454dfccdc041356
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-01-25 22:54:09 +0200

    added onClick / componenDidUpdate / clean-up of state denomination (robots->users)

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import CardList from "../components/CardList";
+import { CardList, CardList2 } from "../components/CardList";
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from "../components/ErrorBoundary";
+import { getUsers, getStarWarsUsers } from "../api/userInfo";
+import ModeButton from '../components/ModeButton';
 // import { robots } from './robots';
 import './App.css';
 
@@ -13,38 +15,75 @@ class App extends React.Component {
         super();
         this.state = {
             // can change
-            robots: [],
-            searchfield: ''
+            users: [],
+            searchfield: '',
+            mode: "robots"
         };
     }
 
     // part of React --> no arrow function
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({ robots: users }));
+    async componentDidMount() {
+        // OLD VERSION:
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        // .then(response => response.json())
+        // .then(users => this.setState({ robots: users }));
+        // let users = [];
+        // if (this.state.mode === "star wars") {
+        //     const data = await getStarWarsUsers();
+        //     users = data.results;
+        //     // console.log(users);
+        // } else if (this.state.mode === "robots") {
+        //     users = await getUsers();
+        //     // console.log(users);
+        // }
+        const users = await getUsers();
+        this.setState({ users: users});
+    }
+
+    async componentDidUpdate(prevProps, prevState) {
+        // let users = this.state.users;
+        if (prevState.mode !== this.state.mode) {
+            let users;
+            if (this.state.mode === "star wars") {
+                const data = await getStarWarsUsers();
+                users = data.results;
+            } else if (this.state.mode === "robots") {
+                users = await getUsers();
+            }
+            this.setState({ users: users});
+        }
+        
+        
     }
 
     onSearchChange = (event) => {
+        // console.log(event);
         this.setState({ searchfield: event.target.value })
+    }
+
+    onClickChange = (event) => {
+        console.log(event.target.innerText.toLowerCase());
+        this.setState({ mode: event.target.innerText.toLowerCase() })
     }
     // classes need a `render()` function
     render() {
-        const { robots, searchfield} = this.state;
-        const filterRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        const { users, searchfield, mode} = this.state;
+        const filterRobots = users.filter(user => {
+            return user.name.toLowerCase().includes(searchfield.toLowerCase())
         });
 
-        if (!robots.length) {
+        if (!users.length) {
             return <h1 className="tc">Loading...</h1>
         } else {
             return (
                 <div className="tc">
                     <h1 className="f2">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
+                    <ModeButton clickChange={this.onClickChange} name="Star Wars"/>
+                    <ModeButton clickChange={this.onClickChange} name="Robots"/>
                     <Scroll>
                         <ErrorBoundary>
-                            <CardList robots={filterRobots}/>
+                            <CardList2 users={filterRobots} mode={mode}/> 
                         </ErrorBoundary>
                     </Scroll>
                 </div>
