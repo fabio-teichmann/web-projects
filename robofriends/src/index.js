5e1262db571ffe83c83b9eb70edba114a214ea0f
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-02-05 16:01:16 +0200

    redux integration for robofriends app

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+
+import thunkMiddleware from 'redux-thunk';
+import { searchRobots, requestRobots } from './reducer';
+
 import App from './containers/App';
+// import App_hooks from './containers/App_hooks';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
 import 'tachyons';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+// applying middleware is ordered
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
